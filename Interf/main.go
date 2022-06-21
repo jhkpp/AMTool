d1d04f87f000f0a4e537135e855c33c2dfbacf6d
JH.K <kog1120@gmail.com>
2022-06-22 00:18:52 +0900

    Create main.go

---
+++ Interf/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "os"
+import "fmt"
+
+type Script_Data struct {
+
+	length int
+}
+
+
+func main() {
+	scripts := os.Args[1:]
+	fmt.Println(scripts)
+
+	for (i := 0; i < 100; i++)
+
+
+	
+	return;
+}
